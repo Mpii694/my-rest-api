0a7b998e1f05bf2f0a0cd72550e78a8f1caa351d
Mpii694 <jbmpiistorereal@gmail.com>
2024-08-03 00:03:32 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,7 +1,7 @@
 const fs = require('fs')
 
-global.creator = 'Zeeoneofc'// yourname
-global.MONGO_DB_URI = "" //database mongodb 
+global.creator = 'MPII OFFC'// yourname
+global.MONGO_DB_URI = "mongodb" //database mongodb 
 global.ACTIVATION_TOKEN_SECRET = "-@Pqnap+@(/1jAPPnew/@10" //isi apa aja bebas
 global.your_email = "" //email
 global.email_password = "" //application password email

60a22f3ad062dc3ddff23552316b6ece3115738b
Mpii694 <jbmpiistorereal@gmail.com>
2025-03-21 04:41:52 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,9 +1,9 @@
 const fs = require('fs')
 
-global.creator = 'Zeeoneofc'// yourname
-global.MONGO_DB_URI = "" //database mongodb 
+global.creator = 'XYNZ STORE'// yourname
+global.MONGO_DB_URI = "mongodb+srv://xynz:<J1d4AmacdQL2COC1>@cluster0.kdabb.mongodb.net/?retryWrites=true&w=majority" //database mongodb 
 global.ACTIVATION_TOKEN_SECRET = "-@Pqnap+@(/1jAPPnew/@10" //isi apa aja bebas
-global.your_email = "" //email
+global.your_email = "yanxmod@gmail.com" //email
 global.email_password = "" //application password email
 global.limitCount = 10000
 global.YUOR_PORT = 8000
